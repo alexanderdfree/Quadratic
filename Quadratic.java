b5206e8cf1a22b9abb57294fa8369807d1aca094
Alex <alexanderdfree@gmail.com>
2020-10-26 20:10:23 -0700

    Update Quadratic.java

--- Quadratic.java
+++ Quadratic.java
@@ -14,7 +14,7 @@ double c){
 		Outputs:
 			return: the y-coordinate at x given a, b, c
 		*/
-      return a*Math.pow(x, 2)+b*x+c;
+      return (a*Math.pow(x, 2)) + (b*x) + c;
 	}
 
 	public static void testQuadY(){
@@ -34,7 +34,7 @@ double c){
 		Outputs:
 			return: the discriminant given a, b, c
       */
-      return ((Math.pow(b, 2))-(4.0 * a *c));
+      return ((Math.pow(b, 2)) - (4.0 * a *c));
 	}
 
 	public static void testDiscriminant(){
@@ -162,15 +162,15 @@ double c){
 	public static double yVertex(double a, double b, double c){
 		/*Find the y-coordinate of the vertex given a, b, c
       Inputs:
-			double x: "x" in y = ax^2 - bx + c
-			double a: "a" in y = ax^2 - bx + c
-			double b: "b" in y = ax^2 - bx + c
-			double c: "c" in y = ax^2 - bx + c
+			double x: "x" in y = ax^2 + bx + c
+			double a: "a" in y = ax^2 + bx + c
+			double b: "b" in y = ax^2 + bx + c
+			double c: "c" in y = ax^2 + bx + c
 		Outputs:
 			return: the y value of the Vertex given a, b, c
 		*/
       double x = (-b)/(2 * a);
-      double y = a*(Math.pow(x,2)) - b*x + c;
+      double y = a*(Math.pow(x,2)) + b*x + c;
       return y; 
       
 	}
@@ -178,8 +178,8 @@ double c){
 	public static void testYVertex(){
 		/*A test client, tests yVertex().
 		*/
-      Testing.testEquals("Test 1", yVertex(1, -2, 1), 0);
-      Testing.testEquals("Test 2", yVertex(-1, 0, 1), 1);
+      Testing.testEquals("Test 1", yVertex(1, -2, 1), 0.0);
+      Testing.testEquals("Test 2", yVertex(-1, 0, 1), 1.0);
       Testing.testEquals("Test 3", yVertex(-1, 3, -5), -2.75);
 	}
 
